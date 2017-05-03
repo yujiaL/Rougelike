744ad037586f01cc656721f29b9006192f92645b
yujiaL <liangy25@csepclab.cs.washington.edu>
2017-05-02 17:28:28 -0700

    Add player movement
    Add player movement

--- source/Player.hx
+++ source/Player.hx
@@ -2,18 +2,84 @@ package source;
 
 import flixel.FlxSprite;
 import flixel.system.FlxAssets.FlxGraphicAsset;
+import flixel.util.FlxColor;
+import flixel.FlxG;
+import flixel.FlxObject;
+import flixel.math.FlxPoint;
 
-class Player 
+class Player extends FlxSprite
 {
 	public var speed:Float = 200;
 	
 	private var _weapon:Weapon;
 	private var _health:Int;
 	
-	public function new(?X:Float=0, ?Y:Float=0, weapon:Weapon) 
+	public function new(?X:Float=0, ?Y:Float=0) 
 	{
 		super(X, Y);
 		makeGraphic(16, 16, FlxColor.PINK);
+		
+		drag.x = drag.y = 1600;
 	}
 	
+	override public function update(elapsed:Float):Void
+	{
+		movement();
+		super.update(elapsed);
+	}
+	
+	private function movement():Void
+	{
+		var _up:Bool = false;
+		var _down:Bool = false;
+		var _left:Bool = false;
+		var _right:Bool = false;
+		
+		_up = FlxG.keys.anyPressed([UP, W]);
+		_down = FlxG.keys.anyPressed([DOWN, S]);
+		_left = FlxG.keys.anyPressed([LEFT, A]);
+		_right = FlxG.keys.anyPressed([RIGHT, D]);
+		
+		if (_up || _down || _left || _right)
+		{
+			if (_up && _down)
+				_up = _down = false;
+			if (_left && _right)
+				_left = _right = false;
+			
+			var mA:Float = 0;
+			if (_up)
+			{
+				mA = -90;
+				if (_left)
+					mA -= 45;
+				else if (_right)
+					mA += 45;
+				facing = FlxObject.UP;
+			}
+			else if (_down)
+			{
+				mA = 90;
+				if (_left)
+					mA += 45;
+				else if (_right)
+					mA -= 45;
+				facing = FlxObject.DOWN;
+			}
+			else if (_left)
+			{
+				mA = 180;
+				facing = FlxObject.LEFT;
+			}
+			else if (_right)
+			{
+				mA = 0;
+				facing = FlxObject.RIGHT;
+			}
+			
+			velocity.set(speed, 0);
+			velocity.rotate(FlxPoint.weak(0, 0), mA);
+			
+		}
+	}
 }
