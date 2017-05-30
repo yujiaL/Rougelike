85c54e2f12df9263947576161321b4b7f016d880
Fengjun Dong <jundong321@gmail.com>
2017-05-30 00:25:42 -0700

    Finalize another sprint

--- source/GlobalVariable.hx
+++ source/GlobalVariable.hx
@@ -5,7 +5,7 @@ class GlobalVariable
 	public static inline var UNIT = 32;
 	public static inline var FONT_SIZE = 32;
 	public static inline var PLAYERHP = 100;
-	public static inline var LOGGING = false;
+	public static inline var LOGGING = true;
 	public static inline var DEBUG = false;
 	public static var REVIVE:Bool;
 }
